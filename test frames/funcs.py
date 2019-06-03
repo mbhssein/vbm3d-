3343cfdf843567df31242bda9acbf0dcb3c77c1d
mohammed <mbhssein@memphis.edu>
2019-06-03 14:50:16 -0500

    revise source destination in sifter and hist_match

--- test frames/funcs.py
+++ test frames/funcs.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import cv2 
 from skimage.exposure import cumulative_distribution
 #from hist_match import hist_match
@@ -7,11 +7,11 @@ from skimage.exposure import cumulative_distribution
 
 
 
-def sifter(template, frame):
+def sifter(window, frame):
     sift = cv2.xfeatures2d.SIFT_create()
 
     # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(template,None)
+    kp1, des1 = sift.detectAndCompute(window,None)
     kp2, des2 =sift.detectAndCompute(frame,None)
     
     FLANN_INDEX_KDTREE = 0
@@ -69,7 +69,7 @@ def pt2blk(arr,x_cntr, y_cntr):
 #        block = arr[xstart:xend, ystart:yend]
 #        
 #        block = arr[xcord:xend, ycord:yend]
-    block = arr[xstart:xstart+blkS-1, ystart:ystart+blkS-1]
+    block = arr[xstart:xstart+blkS, ystart:ystart+blkS]
     return block
 
 def AbuSMatrix(big_one, small_one, starting_point):
@@ -82,49 +82,49 @@ def AbuSMatrix(big_one, small_one, starting_point):
     return big_one
 
 
-import numpy as np
 
-def hist_match(source, template):
-    """
-    Adjust the pixel values of a grayscale image such that its histogram
-    matches that of a target image
 
-    Arguments:
-    -----------
-        source: np.ndarray
-            Image to transform; the histogram is computed over the flattened
-            array
-        template: np.ndarray
-            Template image; can have different dimensions to source
-    Returns:
-    -----------
-        matched: np.ndarray
-            The transformed output image
-    """
-
-    oldshape = source.shape
-    source = source.ravel()
-    template = template.ravel()
-
-    # get the set of unique pixel values and their corresponding indices and
-    # counts
-    s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
-                                            return_counts=True)
-    t_values, t_counts = np.unique(template, return_counts=True)
-
-    # take the cumsum of the counts and normalize by the number of pixels to
-    # get the empirical cumulative distribution functions for the source and
-    # template images (maps pixel value --> quantile)
-    s_quantiles = np.cumsum(s_counts).astype(np.float64)
-    s_quantiles /= s_quantiles[-1]
-    t_quantiles = np.cumsum(t_counts).astype(np.float64)
-    t_quantiles /= t_quantiles[-1]
-
-    # interpolate linearly to find the pixel values in the template image
-    # that correspond most closely to the quantiles in the source image
-    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
-
-    return interp_t_values[bin_idx].reshape(oldshape)
+#def hist_match(source, template):
+#    """
+#    Adjust the pixel values of a grayscale image such that its histogram
+#    matches that of a target image
+#
+#    Arguments:
+#    -----------
+#        source: np.ndarray
+#            Image to transform; the histogram is computed over the flattened
+#            array
+#        template: np.ndarray
+#            Template image; can have different dimensions to source
+#    Returns:
+#    -----------
+#        matched: np.ndarray
+#            The transformed output image
+#    """
+#
+#    oldshape = source.shape
+#    source = source.ravel()
+#    template = template.ravel()
+#
+#    # get the set of unique pixel values and their corresponding indices and
+#    # counts
+#    s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
+#                                            return_counts=True)
+#    t_values, t_counts = np.unique(template, return_counts=True)
+#
+#    # take the cumsum of the counts and normalize by the number of pixels to
+#    # get the empirical cumulative distribution functions for the source and
+#    # template images (maps pixel value --> quantile)
+#    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+#    s_quantiles /= s_quantiles[-1]
+#    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+#    t_quantiles /= t_quantiles[-1]
+#
+#    # interpolate linearly to find the pixel values in the template image
+#    # that correspond most closely to the quantiles in the source image
+#    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+#
+#    return interp_t_values[bin_idx].reshape(oldshape)
 
 def sliding_window2(image, stepSize, windowSize):
 	# slide a window across the image
--- test frames/siftbm.py
+++ test frames/siftbm.py
@@ -3,7 +3,7 @@ import cv2
 from matplotlib import pyplot as plt
 import arf  
 from clairvoyance import img_tools 
-from funcs import sliding_window2, sifter, pt2blk, hist_match, AbuSMatrix, cdf
+from funcs import sliding_window2, sifter, pt2blk, hist_matching, AbuSMatrix, cdf
 import time 
 #from skimage.transform import match_histograms
 import my_PSNR
@@ -14,26 +14,30 @@ winS = 256
 
 t1 = time.clock()
 
-file2 = r'D:\Frames\frames-TM1490202529GR00-4\frames-TM1490202529GR00-4.arf'
+file2 = r'D:\Frames\frames-TM1490202529GR00-4\Frames-tm1490202529gr00-4.arf' 
 arf_obj = arf.read(file2, 'r') 
-img2 = arf_obj.load(932) 
+img2 = arf_obj.load(214) 
 img2 = img_tools.scale_frame_by_percentile(img2,low_pct=1, high_pct=99)*255
 img2 = img2.astype(np.uint8)
 blocks = []
+win_num = 0
 for (x, y, window) in sliding_window2(img2, stepSize=64, windowSize=(winS, winS)):
 		# if the window does not meet our desired window size, ignore it
     if window.shape[0] != winS or window.shape[1] != winS:
          continue
-    print ('mean is', np.mean(window))
+    print ('mean is', np.mean(window))    
+#    if np.mean(window)<70:
+#        
     pts_src, pts_dst = sifter(window, img2)
     for src,dst in zip(pts_src,pts_dst):
         src_blk = pt2blk(window,pts_src[0][0],pts_src[0][1])
         dst_blk = pt2blk(img2, pts_dst[0][0],pts_dst[0][1])
-        matched = hist_match(dst_blk, src_blk)
+#        matched = hist_match(dst_blk, src_blk)
+#        matched = np.zeros([32,32])
 #        matched = match_histograms(dst_blk, src_blk, multichannel=False)
-#        c = cdf (dst_blk)
-#        c_t = cdf (src_blk)
-#        matched = hist_matching(c,c_t,dst_blk)
+        c = cdf (dst_blk)
+        c_t = cdf (src_blk)
+        matched = hist_matching(c,c_t,dst_blk)
         
         new_image = AbuSMatrix(img2, matched, (dst))
     
@@ -47,13 +51,13 @@ plt.imsave('results/TM1490202529GR00_sift_932.png', new_image, cmap = 'gray')
 plt.imsave('results/TM1490202529GR00_orig_932.png', img2     , cmap = 'gray')
 #    img1 = window
 #    pts_src, pts_dst = sifter(img1, img2)
-img_ref= arf_obj.load(1134) 
+img_ref= arf_obj.load(805) 
     
-psnr = my_PSNR.PSNR(img_ref, new_image) 
+psnr = my_PSNR.PSNR(img2, new_image) 
 print('PSNR=',psnr)    
     
     
-t2 = time.clock
+t2 = time.clock()
 
-#print ('took {} seconds'.format(t2 - t1))
+print ('took {} seconds'.format(t2 - t1))
     
